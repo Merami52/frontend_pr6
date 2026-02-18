8f81237f2060ef44b93b5f29483825282e10c9b7
Scarlet <nn814921@gmail.com>
2026-02-18 22:10:42 +0300

    init

---
+++ src/utils/const.js
@@ -0,0 +1,38 @@
+/** Объект настроек для работы с API
+ * @type {{headers: {authorization: string, "Content-Type": string}, serverURL: string}}
+ */
+export const apiSettings = {
+  // Базовый URL сервера, куда будут отправляться запросы
+  serverURL: 'https://mesto.nomoreparties.co/v1/apf-cohort-202',
+  headers: {
+    // Токен авторизации для идентификации пользователя
+    authorization: '90b9f4e4-fe39-4808-89e6-262e3b0f1794',
+    // Указывает, что данные передаются в формате JSON
+    'Content-Type': 'application/json'
+  }
+};
+
+/**
+ * Настройки валидации
+ * @param {string} formSelector - класс формы
+ * @param {string} inputSelector - класс инпута
+ * @param {string} submitButtonSelector - класс кнопки отправки формы
+ * @param {string} inactiveButtonClass - класс, к-рый делает кнопку отправки формы заблокированной
+ * @param {string} inputErrorClass - класс, подсвечивающий поле с ошибками
+ * @param {string} errorClass - класс, делающий ошибку видимой
+ */
+/** @type {Object} */
+export const validationSettings = {
+  // CSS-селектор формы, которую нужно валидировать
+  formSelector: '.popup__form',
+  // CSS-селектор для инпутов в форме
+  inputSelector: '.popup__input',
+  // CSS-селектор для кнопки отправки формы
+  submitButtonSelector: '.popup__submit',
+  // Класс, применяемый к кнопке отправки, чтобы сделать её неактивной
+  inactiveButtonClass: 'popup__submit_disabled',
+  // Класс, добавляемый инпутам при возникновении ошибки
+  inputErrorClass: 'popup__input_type_error',
+  // Класс, который делает текст ошибки видимым для пользователя
+  errorClass: 'popup__error_visible'
+};
